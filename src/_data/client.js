c7aca02057341ae0e3bfc96cc301e93def2dd44a
Danny Skelley <dannyskelley@gmail.com>
2024-10-01 09:31:15 -0400

    a
    a

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,15 +1,15 @@
 module.exports = {
     "name": "Stay True Tattoo - St. Augustine Florida",
-    "email": "info@tomatotattoo.com",
+    "email": "",
     "phoneForTel": "904-217-4413",
     "phoneFormatted": "904.217.4413",
     "address": {
-        "lineOne": "1205 Maple Ave",
+        "lineOne": "199 West King Street",
         "lineTwo": "",
-        "city": "Lisle",
-        "state": "IL",
-        "zip": "60532",
-        "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2974.9660365507325!2d-88.0751324!3d41.785945600000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e5120cc03adc1%3A0x9cdb5fe6f3de2b89!2s1205%20Maple%20Ave%2C%20Lisle%2C%20IL%2060532!5e0!3m2!1sen!2sus!4v1718991268043!5m2!1sen!2sus"
+        "city": "St. Augustine",
+        "state": "FL",
+        "zip": " 32084 ",
+        "mapLink": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3459.0818211080696!2d-81.32874242399313!3d29.890742925835962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e427e92af54ce7%3A0x7b63bbd4270920ee!2sStay%20True%20Tattoo!5e0!3m2!1sen!2sus!4v1727788536334!5m2!1sen!2sus"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
